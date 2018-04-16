2167aa31961a80e900cc99ed53604dd25636a1b8
Alina Iuliana Caia <37437952+alinaic@users.noreply.github.com>
2018-04-16 13:04:16 +0200

    Add files via upload

---
+++ Java35asociacionDatos/src/servlet/ServletListadoProductos.java
@@ -0,0 +1,44 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import modelo.Producto;
+import daos.ProductosDAO;
+import daosImpl.ProductosDAOImpl;
+
+
+@WebServlet("/ServletListadoProductos")
+public class ServletListadoProductos extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String desde = request.getParameter("desde");
+		String cuantos = request.getParameter("cuantos");
+		int desdeInt = 0;
+		int cuantosInt = 10;
+		if(desde!=null && cuantos!=null){
+			desdeInt = Integer.parseInt(desde);
+			cuantosInt = Integer.parseInt(cuantos);
+		}
+		
+		
+		ProductosDAO dao = new ProductosDAOImpl();
+		System.out.println("desde: " + desdeInt + " cuantos: "+cuantosInt);
+		List<Producto> productos = dao.obtenerProductos(desdeInt,cuantosInt);
+		request.setAttribute("productos", productos);
+		request.setAttribute("productosTotales", dao.obtenerTotalProductos());
+		request.setAttribute("desdeSiguiente", desdeInt + 20);
+		request.setAttribute("desdeAnterior", desdeInt - 20);
+		
+		request.getRequestDispatcher("listadoProductos.jsp").forward(request, response);
+	}
+
+}
